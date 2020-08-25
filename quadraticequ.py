1f85d710ac1e26ea4566225a654d891ff36bbf24
LL <LL@163.com>
2020-08-25 15:51:49 +0800

    second commit

---
+++ quadraticequ.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import math
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
+d = b**2 - 4 * a *c
+if d < 0:
+    print("ROOTS are imaginary")
+else:
+    root1 = (-b + math.sqrt(d)) / (2 * a)
+    root2 = (-b - math.sqrt(d)) / (2 * a)
+    print("Root 1 = {},Root 2 = {}".format(root1,root2))
